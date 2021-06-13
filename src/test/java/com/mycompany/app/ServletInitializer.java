42f03c35acdac7c1dcfa38c1a36227fb82d38551
Joe-Imman <81916022+Joe-Imman@users.noreply.github.com>
2021-06-13 15:00:00 +0530

    Update ServletInitializer.java

--- src/test/java/com/mycompany/app/ServletInitializer.java
+++ src/test/java/com/mycompany/app/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(Application.class);
+        return builder.sources(AppTest.class);
     }
 }
  
